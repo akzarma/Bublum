c6be48c8b857095645fcd8d0044ec2b74bface7a
rahul <rshahshah2890@gmail.com>
2018-12-16 12:30:29 +0530

    merge

--- violation_detection/object_detection/config.py
+++ violation_detection/object_detection/config.py
@@ -1,7 +1,7 @@
 import os
 
 model = {
-    'name': 'ssd_resnet50',
+    'name': 'helmet_graph',
     'graph_name': 'frozen_inference_graph.pb',
     'download_base_url': 'http://download.tensorflow.org/models/object_detection/',
     'label': os.path.join('labels', 'label_map.pbtxt')
@@ -15,17 +15,17 @@ to_download = False
 maximum_classes_to_detect = 90
 
 path = {
-    'image_dir': os.path.join('input', 'images', 'aks'),
+    'image_dir': os.path.join('input', 'images', 'cut_motor'),
     'video_dir': os.path.join('input', 'videos'),
     'video_name': 'final1.mp4',
     'output_image_name': 'my_test',
     'output_video_name': 'final_django.avi',
-    'output_image_dir': os.path.join('output', 'images', 'rc4'),
+    'output_image_dir': os.path.join('output', 'images', 'helmet_final'),
     'output_video_dir': os.path.join('output', 'videos', 'django'),
 }
 
-is_image = False
-generate_video = True
+is_image = True
+generate_video = False
 # Size, in inches, of the output images.
 image_size = (12, 8)
 
@@ -36,7 +36,7 @@ save_by_class = True
 
 minimum_detection = 20
 
-detect_all_categories = False
+detect_all_categories = True
 show_labels = True
 
 categories = ['car', 'motorcycle', 'airplane', 'bus', 'truck', 'traffic light', 'person']
