d1d9a04765729c8232be15d5e67d269ac833dedb
shibashis <shibashismallik@gmail.com>
2018-12-16 09:09:46 +0530

    Progress bar UI.

--- violation_detection/Interface/views.py
+++ violation_detection/Interface/views.py
@@ -38,6 +38,7 @@ def vehicle_model(request):
     detection_graph = tf.Graph()
     with detection_graph.as_default():
         od_graph_def = tf.GraphDef()
+        # print(os.path.exists(model['path']))
         with tf.gfile.GFile(model['path'], 'rb') as fid:
             serialized_graph = fid.read()
             od_graph_def.ParseFromString(serialized_graph)
